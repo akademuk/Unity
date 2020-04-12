73cb7d45de37c936fe49843cff9772b25e968388
Oleh Fedorov <akademuk24@gmail.com>
2020-04-12 20:52:08 +0300

    first commit

---
+++ scripts/burger-phone.js
@@ -0,0 +1,7 @@
+document.getElementById("trigger-phone").onclick = function () {
+  openPhone();
+};
+
+function openPhone() {
+  document.getElementById("menu-phone").classList.toggle("show-phone");
+}
